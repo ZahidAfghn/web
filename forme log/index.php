c53570944c039845108e5a23b378b9fb49e30381
ZahidAfghn <111078253+ZahidAfghn@users.noreply.github.com>
2024-01-26 10:31:21 -0800

    Create index.php
    ert67rer

---
+++ forme log/index.php
@@ -0,0 +1,22 @@
+<!Document html>
+html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<H1>hello</H1>
+
+
+<div  class container>
+<form class="box" action="6php.php"method="POST">
+    <div>
+       <label for=""></label> 
+       <input type="text" name="name"placeholder="entre your name">
+    </div>
+    </form>
+</div>
+</body>
+</html>
